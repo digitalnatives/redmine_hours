9145e5e179fde40b421a782b14197a296aed0fbf
zsbalogh <baloghzsof@gmail.com>
2012-08-30 10:38:55 +0200

    Closes #5. Dropdown now doesn't contain closed issues.

--- init.rb
+++ init.rb
@@ -11,6 +11,11 @@ Rails.configuration.to_prepare do
     named_scope :for_user, lambda { |user| {:conditions => "#{TimeEntry.table_name}.user_id = #{user.id}"}}
     named_scope :spent_on, lambda { |date| {:conditions => ["#{TimeEntry.table_name}.spent_on = ?", date]}}
   end
+  Project.class_eval do
+    def open_issues
+      self.issues.reject(&:closed?)
+    end
+  end
 end
 
 
